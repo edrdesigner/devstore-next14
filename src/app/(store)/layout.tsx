0d7f474d5631a82421857ee2c4acc25abd7016bc
Eduardo Reichert <eduardo.reichert@totvs.com.br>
2023-12-26 10:16:59 -0300

    chore: Contexto do carrinho

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Header } from '@/components/header'
+import { CartProvider } from '@/context/cart-context'
 
 export interface StoreLayoutProps {
   children: React.ReactNode
@@ -7,9 +8,11 @@ export interface StoreLayoutProps {
 
 export default function StoreLayout({ children }: StoreLayoutProps) {
   return (
-    <div className="mx-auto grid min-h-screen max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }

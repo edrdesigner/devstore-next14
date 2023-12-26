9adc1491aedc94e2ae39b5bf0682578179d7bfaf
Eduardo Reichert <eduardo.reichert@totvs.com.br>
2023-12-26 14:26:01 -0300

    chore: Busca

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -5,6 +5,8 @@ import data from '../data.json'
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl
 
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const query = z.string().parse(searchParams.get('q'))
 
   const products = data.products.filter((product) => {
